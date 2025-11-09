36843feba3d726db7e004024541a25635d18b45d
Eukarya-est <pdg2619@gmail.com>
2025-11-09 18:04:06 +0900

    #6 add 'copy' button

--- assets/resume.js
+++ assets/resume.js
@@ -1,1 +1,10 @@
 
+  function copyAddress() {
+    navigator.clipboard.writeText("pdg2619@gmail.com")
+      .then(() => {
+        alert("Code copied to clipboard!");
+      })
+      .catch(err => {
+        console.error("Failed to copy code: ", err);
+      });
+  }
